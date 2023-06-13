ee0a2f991b6ce4e1cbd2db9eff115af6d434fa65
oryuhsan <c0a2215278@edu.teu.ac.jp>
2023-06-13 16:59:43 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -11,18 +11,27 @@ def main():
     #こうかとんの画像読み込み
     koukaton = pg.transform.flip(koukaton, True, False)
     #こうかとん反転
-    kaiten= pg.transform.rotozoom(koukaton,10,1.0)
+    kaiten= pg.transform.rotozoom(koukaton,0,1.0)
     #こうかとん回転
     kkflip=[koukaton,kaiten]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [(0-tmr%1600),0])
-        #screen.blit(bg_img, [(1600-tmr%1600),0])
+        if 10<=tmr%100<20 or 30<=tmr%100<40 or 50<=tmr%100<60 or 70<=tmr%100<80 or 90<=tmr%100<100:  
+            kaiten= pg.transform.rotozoom(koukaton,10-tmr%10,1.0)
+        else:
+            kaiten = pg.transform.rotozoom(koukaton,tmr%10,1.0)
+        screen.blit(bg_img, [-tmr,0])
+        if tmr%1600 == 0:
+            hanten= pg.transform.flip(bg_img, True, False)
+        screen.blit(hanten, [(1600-tmr),0])
+        screen.blit(bg_img, [3200-tmr,0])
         #こうかとんが羽ばたく
-        screen.blit(kkflip[tmr%2],[300,200])
+        screen.blit(kaiten,[300,200])
         pg.display.update()
+        if tmr == 3200:
+            tmr = 0
         tmr += 1        
         clock.tick(100)
 
