33f9dc6ec8478c66076bf896bf6b4d3e85503543
oryuhsan <c0a2215278@edu.teu.ac.jp>
2023-06-13 15:25:30 +0900

    2問目

--- pygame_image.py
+++ pygame_image.py
@@ -15,10 +15,7 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
         screen.blit(bg_img, [0, 0])
-        screen.blit(koukaton,[0,0])
-        #こうかとんのsurface生成
         pg.display.update()
         tmr += 1        
         clock.tick(10)
